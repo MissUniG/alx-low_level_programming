47f8aa058369fe9cd56fb54b38a2aa60517e87ec
MissUniG <111026483+MissUniG@users.noreply.github.com>
2022-09-13 20:40:28 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
- * main - Entry point
- *
- * Return: Always 0
- */
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
 _putchar('_');
@@ -15,6 +16,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
+
 return (0);
 }
-
